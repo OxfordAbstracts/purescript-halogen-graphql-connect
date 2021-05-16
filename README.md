3622de4a4d3365fa1e7f87c30608a9b3e2deadac
roryc89 <roryc89@googlemail.com>
2021-05-16 21:04:23 +0100

    adds useMutationFullRes

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Make graphQL calls in your halogen project, using hooks or higher order components. 
 
+## Graphql Hooks 
+
+This is probably the easiest way to use this library
+TODO: add docs
+
--- src/Halogen/GraphQL/Hooks/Mutation.purs
+++ src/Halogen/GraphQL/Hooks/Mutation.purs
@@ -1,15 +1,16 @@
-module Halogen.GraphQL.Hooks.Mutation (useMutation) where
+module Halogen.GraphQL.Hooks.Mutation (useMutation, useMutationFullRes) where
 
 import Prelude
 
 import Data.Argonaut (Json, JsonDecodeError)
 import Data.Either (Either, either)
 import Effect.Aff.Class (class MonadAff, liftAff)
+import GraphQL.Client.Query (getFullRes)
 import GraphQL.Client.SafeQueryName (safeQueryName)
 import GraphQL.Client.ToGqlString (toGqlQueryString)
-import GraphQL.Client.Types (class GqlQuery, class QueryClient, Client(..), clientMutation, defMutationOpts)
-import Halogen.GraphQL.Error (GqlFailure(..))
+import GraphQL.Client.Types (class GqlQuery, class QueryClient, Client(..), GqlRes, clientMutation, defMutationOpts)
 import Halogen.GraphQL.GqlRemote (GqlRemote)
+import Halogen.GraphQL.Internal.Util (checkErrorsAndDecode)
 import Halogen.Hooks (HookM)
 import Network.RemoteData (RemoteData(..))
 
@@ -26,8 +27,26 @@ useMutation ::
   HookM m (GqlRemote res)
 useMutation decoder optsF queryNameUnsafe query (Client client) = do
   json <- liftAff $ clientMutation opts client queryName $ toGqlQueryString query
-  pure $ either (Failure <<< DecodeError) pure (decoder json)
+  pure $ either Failure pure (checkErrorsAndDecode true decoder json)
+  where 
+  opts = optsF (defMutationOpts client)
 
+  queryName = safeQueryName queryNameUnsafe
+
+useMutationFullRes ::
+  forall client qOpts opts query m res qSchema mSchema sSchema.
+  QueryClient client qOpts opts =>
+  MonadAff m =>
+  GqlQuery mSchema query res =>
+  (Json -> Either JsonDecodeError res) ->
+  (opts -> opts) ->
+  String ->
+  query ->
+  Client client qSchema mSchema sSchema ->
+  HookM m (GqlRes res)
+useMutationFullRes decoder optsF queryNameUnsafe query (Client client) = do
+  json <- liftAff $ clientMutation opts client queryName $ toGqlQueryString query
+  pure $ getFullRes decoder json
   where 
   opts = optsF (defMutationOpts client)
 
